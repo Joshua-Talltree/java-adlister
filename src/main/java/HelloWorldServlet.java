734468f59b5bb0bcadd5f0fe46cd8c1014a4b1bf
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-03-04 14:14:28 -0600

    Hello world servlet with counter and name change query string.

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,26 @@
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+    int pageViewCount;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String youVisited = "You visited my page:";
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        pageViewCount++;
+        if (name != null) {
+            pageViewCount = 0;
+            out.printf("<h1>Hello %s </h1>", name);
+        } else {
+            out.println("<h1>Hello, World</h1>");
+        }
+        out.print("<h2>" + youVisited + "<h2>" + "<h2>" + pageViewCount + "</h2>" + "<h2>times!</h2>");
     }
 }
